741c071f37cc5f5dd1955ad8167d7c37aa09fb6c
Stephan Richter <stephan.richter@gmail.com>
2004-08-12 20:03:58 +0000

    Backported
    r26567 | srichter | 2004-07-16 02:58:27 -0400 (Fri, 16 Jul 2004) | 2 lines
    r26570 | srichter | 2004-07-16 03:54:40 -0400 (Fri, 16 Jul 2004) | 3 lines

---
+++ interfaces.py
@@ -0,0 +1,32 @@
+##############################################################################
+#
+# Copyright (c) 2003 Zope Corporation and Contributors.
+# All Rights Reserved.
+# 
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+# 
+##############################################################################
+"""Zope-specific HTTP interfaces
+
+$Id$
+"""
+
+from zope.interface import Interface, Attribute
+
+class INullResource(Interface):
+    """Placeholder objects for new container items to be created via PUT
+    """
+
+    container = Attribute("The container of the future resource")
+    name = Attribute("The name of the object to be created.")
+
+class IHTTPException(Interface):
+    """Marker interface for http exceptions views
+    """
+    pass
+
