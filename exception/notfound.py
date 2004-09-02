28406445705b0570d213127e4e9c23b8a246ae3f
Phil Ruggera <pruggera@san.rr.com>
2004-09-02 07:05:38 +0000

    Update doc strings to ReST

--- exception/notfound.py
+++ exception/notfound.py
@@ -2,32 +2,39 @@
 #
 # Copyright (c) 2003 Zope Corporation and Contributors.
 # All Rights Reserved.
-# 
+#
 # This software is subject to the provisions of the Zope Public License,
 # Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
 # WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
-# 
+#
 ##############################################################################
-"""Zope-specific HTTP interfaces
+"""Not found Exception
 
 $Id$
 """
 __docformat__ = 'restructuredtext'
 
-from zope.interface import Interface, Attribute
+from zope.app.http.interfaces import IHTTPException
+from zope.interface import implements
 
-class INullResource(Interface):
-    """Placeholder objects for new container items to be created via PUT
-    """
+class NotFound(object):
 
-    container = Attribute("The container of the future resource")
-    name = Attribute("The name of the object to be created.")
+    implements(IHTTPException)
 
-class IHTTPException(Interface):
-    """Marker interface for http exceptions views
-    """
-    pass
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
 
+    def __call__(self):
+        if self.request.method in ['MKCOL'] and \
+               self.request.getTraversalStack():
+            # MKCOL with non-existing parent.
+            self.request.response.setStatus(409)
+        else:
+            self.request.response.setStatus(404)
+        return ''
+
+    __str__ = __call__
--- exception/unauthorized.py
+++ exception/unauthorized.py
@@ -2,32 +2,34 @@
 #
 # Copyright (c) 2003 Zope Corporation and Contributors.
 # All Rights Reserved.
-# 
+#
 # This software is subject to the provisions of the Zope Public License,
 # Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
 # WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
-# 
+#
 ##############################################################################
-"""Zope-specific HTTP interfaces
+"""Unauthorized Exception
 
 $Id$
 """
 __docformat__ = 'restructuredtext'
 
-from zope.interface import Interface, Attribute
+from zope.app.http.interfaces import IHTTPException
+from zope.interface import implements
 
-class INullResource(Interface):
-    """Placeholder objects for new container items to be created via PUT
-    """
+class Unauthorized(object):
 
-    container = Attribute("The container of the future resource")
-    name = Attribute("The name of the object to be created.")
+    implements(IHTTPException)
 
-class IHTTPException(Interface):
-    """Marker interface for http exceptions views
-    """
-    pass
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
 
+    def __call__(self):
+        self.request.unauthorized("basic realm='Zope'")
+        return ''
+
+    __str__ = __call__
