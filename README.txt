83f5115265da456697e763e81f70d038fa7cca14
Stephan Richter <stephan.richter@gmail.com>
2007-10-24 19:16:25 +0000

    Get ready for release. There was a ftest that was not being run.

---
+++ README.txt
@@ -0,0 +1,3 @@
+This package implements the simplest HTTP behavior within the Zope
+Puyblisher. It implements all HTTP verbs as views and defines the necessary
+HTTP exceptions.
--- setup.py
+++ setup.py
@@ -15,21 +15,42 @@
 
 $Id$
 """
-
 import os
+from setuptools import setup, find_packages
 
-from setuptools import setup, find_packages, Extension
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.http',
-      version = '3.4.0b1',
-      url='http://svn.zope.org/zope.app.http',
-      license='ZPL 2.1',
+      version = '3.4.0',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
-	  packages=find_packages('src'),
-	  package_dir = {'': 'src'},
+      description='HTTP Behavior for the Zope Publisher',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 http publisher view",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.app.http',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
       namespace_packages=['zope', 'zope.app'],
-      extras_require=dict(test=['zope.app.testing']),
+      extras_require=dict(
+          test=['zope.app.testing',
+                'zope.app.securitypolicy',
+                'zope.app.file']),
       install_requires=['setuptools',
                         'zope.interface',
                         'zope.app.zapi',
@@ -39,6 +60,5 @@ setup(name='zope.app.http',
                         'zope.filerepresentation',
                         ],
       include_package_data = True,
-
       zip_safe = False,
       )
