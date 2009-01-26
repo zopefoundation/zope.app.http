134a8491fb8b3aef850a39d7c238930ec0c9fa37
Hanno Schlichting <hanno@hannosch.eu>
2009-01-26 20:16:46 +0000

    Prepare 3.4.2

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.http',
-      version = '3.4.2dev',
+      version = '3.4.2',
       author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
+      author_email='zope-dev@zope.org',
       description='HTTP Behavior for the Zope Publisher',
       long_description=(
           read('README.txt')
@@ -42,7 +42,7 @@ setup(name='zope.app.http',
           'Operating System :: OS Independent',
           'Topic :: Internet :: WWW/HTTP',
           'Framework :: Zope3'],
-      url='http://cheeseshop.python.org/pypi/zope.app.http',
+      url='http://pypi.python.org/pypi/zope.app.http',
       license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
